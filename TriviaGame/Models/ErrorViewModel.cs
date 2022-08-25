9fd2b8f450d04f37418d3b56f53df74f3f650dd5
paulrndp <paulr@DESKTOP-47REJSO>
2022-08-25 18:55:56 +0800

    Add project files.

---
+++ TriviaGame/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TriviaGame.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
